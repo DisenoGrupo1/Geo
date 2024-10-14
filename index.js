fe61e3207c9161c5a05bf5f7cd8dd17b00159023
Dylanmaump30 <dylanmauricio05@gmail.com>
2024-10-14 09:59:50 -0500

    Change marker icon to a taxi icon

--- index.js
+++ index.js
@@ -72,10 +72,7 @@ function initializeWebSocket() {
                         position: lastLatLng,
                         map: map,
                         icon: {
-                            url: "http://geotaxi.ddns.net/icon/taxi.png", // Ruta a tu imagen de taxi
-                            scaledSize: new google.maps.Size(50, 50), // Tamaño del icono
-                            origin: new google.maps.Point(0, 0), // Origen de la imagen
-                            anchor: new google.maps.Point(25, 25) // Punto de anclaje del icono
+                            url: "https://maps.gstatic.com/mapfiles/ms2/micons/cabs.png", // Ruta a tu imagen de taxi
                         }   
                     });
                     map.setCenter(lastLatLng);
